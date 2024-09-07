96c4dadeb08ed2481bffc70c85f6272c6630c335
mohammed afzal <85404791+afzal631@users.noreply.github.com>
2024-09-07 15:59:37 +0530

    cors update

--- server.js
+++ server.js
@@ -21,7 +21,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin:"https://cloth-mart-frontend.vercel.app/",
+    origin: [
+      "http://localhost:5173",
+      "https://cloth-mart-frontend.vercel.app/",
+    ],
     credentials: true,
   })
 );
