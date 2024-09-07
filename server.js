34c37eb102d45301485c240863e69669b9aa7192
mohammed afzal <85404791+afzal631@users.noreply.github.com>
2024-09-07 19:44:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,20 +14,20 @@ const port = process.env.PORT;
 
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://cloth-mart-frontend.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: "25mb" }));
 // app.use(express.urlencoded({ limit: "25mb" }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://cloth-mart-frontend.vercel.app/",
-    ],
-    credentials: true,
-  })
-);
 
 connectdb()
   .then(() => console.log("mongodb connection established successfully."))
